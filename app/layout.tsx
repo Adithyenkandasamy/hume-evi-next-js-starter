2d9d44f1e7d7e9371323a0d6c6b132427e5544a9
Adithyenkandasamy <adithyen1@gmail.com>
2024-12-29 14:39:01 +0530

    2:38

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          GeistSans.variable,
           GeistMono.variable,
+          GeistSans.variable,
           "flex flex-col min-h-screen"
         )}
       >
